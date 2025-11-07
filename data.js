b51f9ecc0db34fd4d35d09b2be535e180ac2b415
dohasid <sidahmeddoha@gmail.com>
2025-11-06 22:39:59 -0800

    Delete data.js

--- data.js
+++
@@ -1,8 +0,0 @@
-const letterFrequencies = ['E', 'T', 'A', 'O', 'I', 'N', 'S', 'H', 'R', 'D', 'L', 'C', 'U', 'M', 'W', 'F', 'G', 'Y', 'P', 'B', 'V', 'K', 'J', 'X', 'Q', 'Z'];
-
-const nextLetterMap = {
-    T: ['H', 'R', 'O'],
-    H: ['E', 'A', 'I'],
-    A: ['N', 'T', 'S'],
-    // etc...
-};
